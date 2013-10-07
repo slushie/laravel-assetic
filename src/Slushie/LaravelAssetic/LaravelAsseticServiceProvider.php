94deb93a118df163a757c64016351d03ca8a73c6
Josh Leder <josh@ha.cr>
2013-10-07 11:15:06 -0600

    initial commit of assetic package

---
+++ src/Slushie/LaravelAssetic/LaravelAsseticServiceProvider.php
@@ -0,0 +1,46 @@
+<?php namespace Slushie\LaravelAssetic;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAsseticServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('slushie/laravel-assetic');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['asset'] = $this->app->share(function() {
+      return;
+    });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('asset');
+	}
+
+}
