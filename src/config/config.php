1539d245c6b4e85ec4c92c67b6c1c248f32af8aa
Tony Li <tonyli.lives@gmail.com>
2015-02-02 11:59:19 -0800

    cleaned up config file to be easy to read

--- src/config/config.php
+++ src/config/config.php
@@ -1,120 +1,92 @@
 <?php
-/**
- * Configure laravel-assetic in this file.
- * File modified by AARYAN ADITYA
- * @package slushie/laravel-assetic
- */
 
 return array(
-  /*
-   * Groups are named settings containing assets and filters,
-   * plus an output file.
-   */
-  'groups' => array(
+    /*
+    |--------------------------------------------------------------------------
+    | Groups
+    |--------------------------------------------------------------------------
+    |
+    | Groups of assets to run over a set of filters into an output file.
+    | By default, all paths to files begin in the public_path() directory.
+    | The order of asset definition is maintained in the output file.
+    |
+    | Use the Asset::url() function to generate the link to the final file:
+    | <script src="<?php echo Asset::url('main_js'); ?>"></script>
+    |
+     */
 
-      /*
-      * First create a group then add scripts into group.. 
-      * if you want add folder then mention folderpath/*extension
-      * By Default it will take public path.. you can also mention base path.
-      * mention output file name  where scripts should compile...
-      * finally add group name in views as
-      * <script src="<?php echo Asset::url('singlejs-frontend'); ?>"></script>
-      */
-
-    //This group associated with javascripts of front-end website
-
-     /*
-     @external scrpt call in views :
-     <script src="<?php echo Asset::url('singlejs-frontend'); ?>"></script>
-     */ 
-
-    'singlejs-main' => array(
-      // named filters are defined below
-      'filters' => array(
-        'js_min' // Here you need to mention filter name
-      ),
-
-      // named assets defined below
-      'assets' => array(
-        'assets/javascripts/coolarize/*js',
-        'assets/javascripts/common/search.js',
-        'assets/javascripts/nect/next.js'
-        // its also possible to include assets here directly
-        // eg, public_path('jquery-ui.js')
-      ),
-
-      // output path (probably relative to public)
-      // must be rewritable
-      'output' => 'singlejs-main.js'
+    'groups' => array(
+        'main_js' => array(
+            'filters' => array(
+                'js_min',
+            ),
+            'assets' => array(
+                'jquery',                       // Named asset defined below
+                'assets/js/common/search.js',   // Single file
+                'assets/js/coolarize/*js',      // Folder inclusion
+            ),
+            'output' => 'scripts.js',           // Writable output relative to public_path()
+        ),
+        'main_css' => array(
+            'filters' => array(
+                'css_import',
+                'css_rewrite',
+                'css_min',
+            ),
+            'assets' => array(
+                'assets/css/bootstrap/css/bootstrap.min.css',
+                'assets/css/font-awesome/css/font-awesome.css',
+                'assets/css/prettyPhoto/css/*css',
+            ),
+            'output' => 'vendors.css'
+        ),
+        'main_less' => array(
+            'filters' => array(
+                'css_import',
+                'css_rewrite',
+                'less_php',
+                'css_min'
+            ),
+            'assets' => array(
+                'assets/less/master.less'
+            ),
+            'output' => 'styles.css'
+        ),
     ),
 
-     // Adding css to singlecss-main group
+    /*
+    |--------------------------------------------------------------------------
+    | Filters
+    |--------------------------------------------------------------------------
+    |
+    | Name => class key-values for filters to use.
+    | The use of closure based filters are also possible.
+    |
+     */
 
-    'singlecss-main' => array(
-      // you define multiple filters in array
-      'filters' => array(
-        'css_import',
-        'css_rewrit',
-        'css_min'
-      ),
-
-      // named assets defined below
-      'assets' => array(
-        'assets/stylesheets/frontend/font-awesome/css/font-awesome.css',
-        'assets/stylesheets/frontend/prettyPhoto/css/*css',
-        'assets/stylesheets/frontend/bootstrap/css/bootstrap.min.css'
-
-        // its also possible to include assets here directly
-        // eg, public_path('jquery-ui.js')
-      ),
-
-      // output path (probably relative to public)
-      // must be rewritable
-      'output' => 'singlecss-main.css'
+    'filters' => array(
+        'css_min'       => 'Assetic\Filter\CssMinFilter',
+        'css_import'    => 'Assetic\Filter\CssImportFilter',
+        'css_rewrite'   => 'Assetic\Filter\CssRewriteFilter',
+        'embed_css'     => 'Assetic\Filter\PhpCssEmbedFilter',
+        'less_php'      => 'Assetic\Filter\LessphpFilter',
+        'js_min'        => 'Assetic\Filter\JSMinFilter',
+        'coffee_script' => 'Assetic\Filter\CoffeeScriptFilter',
+        'yui_js' => function () {
+            return new Assetic\Filter\Yui\JsCompressorFilter('yui-compressor.jar');
+        },
     ),
 
-    
-    // Adding less files to bast-public-less group
+    /*
+    |--------------------------------------------------------------------------
+    | Named Assets
+    |--------------------------------------------------------------------------
+    |
+    | Name => path key-values for common files to be included in groups.
+    |
+     */
 
-    'bast-public-less' => array(
-      // named filters are defined below
-      'filters' => array(
-        'css_import',
-        'css_rewrit',
-        'less_php',
-        'css_min'
-      ),
-
-      // named assets defined below
-      'assets' => array(
-        'assets/css/less/master.less'
-      ),
-
-      // output path (probably relative to public)
-      // must be rewritable
-      'output' => 'bast-public-less.css'
-    ),
-
-  ),
-
-  'filters' => array(
-    // filter with a closure constructor
-    'yui_js' => function() {
-      return new Assetic\Filter\Yui\JsCompressorFilter('yui-compressor.jar');
-    },
-
-    // filter with a simple class name
-    'js_min'      => 'Assetic\Filter\JSMinFilter',
-    'css_import'  => 'Assetic\Filter\CssImportFilter',
-    'css_min'     => 'Assetic\Filter\CssMinFilter',
-    'css_rewrit'  => 'Assetic\Filter\CssRewriteFilter',
-    'emed_css'    => 'Assetic\Filter\PhpCssEmbedFilter',
-    'coffe_script'=> 'Assetic\Filter\CoffeeScriptFilter',
-    'less_php'    => 'Assetic\Filter\LessphpFilter',
-  ),
-
-  'assets' => array(
-    // name => absolute path to asset file
-    // 'jquery' => public_path('script/jquery.js'),
-  )
+    'assets' => array(
+        'jquery' => 'assets/javascripts/jquery.js',
+    )
 );
